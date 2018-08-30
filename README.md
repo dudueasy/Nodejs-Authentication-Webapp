0907508a1eb097d147b0f9e750cce83ce07d5f59
Apolo <apolo@192.168.0.100>
2018-08-31 06:05:52 +0800

    define explicit validation rules; render error to template

--- README.md
+++ README.md
@@ -4,8 +4,17 @@
 # 工程结构
 webpack + nodemon
 
+# 模板引擎
+handlerbars
 
 # 项目依赖
+mysqljs/mysql
+dotenv 
+
+# 实现
+## 表单验证
+通过 express-validator 和 数据库索引 实现后台表单验证.
+
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -7,27 +7,37 @@ router.get('/register', function(req, res, next) {
   res.render('register', { title: 'registration' });
 });
 
-router.post('/register', 
-  [check('username').isLength({min: 5}), check('email').isEmail(), check('password').isLength({min:5}) ]
+router.post(
+  '/register', 
+  [
+    check('username','username must be between 8-16 characters').isLength({min: 8, max: 16}), 
+    check('email').isEmail().withMessage('please input a valid email'),
+    check('email').isLength({min:8, max:100}).withMessage('email must be between 8-100 characters'),
+    check('password').isLength({min:5, max:100}).withMessage('password should be at lease 5 characters'), 
+    check('passwordMatch').equals('password').withMessage('password don"t match'), ]
   , 
   (req, res, next)=>{
   let {username, email, password, passwordMatch} = req.body 
   console.log(username, email, password, passwordMatch)
 
-  // form data validation
-  const errors = validationResult(req); 
-  if (!errors.isEmpty()) {
-    console.log(errors.array())
-    return res.render('register', {title:'registration failed'});
-  }
+  // get form data validation result
+    const errors = validationResult(req); 
+    validationErrors = errors.array()
+    if (!errors.isEmpty()) { 
+      return res.render('register', {title:'registration failed', errors: validationErrors})
+    }   
 
-  // query starts here
-  const connection = require('../db.js') 
-  let query = `INSERT INTO users (username, email, password) VALUES (?,?,?)` 
-  connection.query(query, [username, email, password], (err, results, fields)=>{
-    if(err) throw err; 
-    res.render('register', { title: 'registration completed' }); 
-  }) 
-});
+
+    // query starts here
+    const connection = require('../db.js') 
+    let query = `INSERT INTO users (username, email, password) VALUES (?,?,?)` 
+    connection.query(query, [username, email, password], (err, results, fields)=>{
+      if(err) { 
+        console.log('error happen during query:',err); 
+        res.render('register', { title: 'registration failed' }); 
+      } 
+      res.render('register', { title: 'registration completed' }); 
+    }) 
+  });
 
 module.exports = router;
