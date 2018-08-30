1ea93303ad3834c3a11fc725c2c83429e6670439
Apolo <apolo@192.168.0.100>
2018-08-31 04:57:11 +0800

    apply express-validator for form data validation

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,29 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router(); 
+const { check, validationResult } = require('express-validator/check');
 
 /* GET home page. */
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'registration' });
 });
 
-router.post('/register', function(req, res, next) {
+router.post('/register', 
+  [check('username').isLength({min: 5}), check('email').isEmail(), check('password').isLength({min:5}) ]
+  , 
+  (req, res, next)=>{
   let {username, email, password, passwordMatch} = req.body 
   console.log(username, email, password, passwordMatch)
 
-  const connection = require('../db.js')
+  // form data validation
+  const errors = validationResult(req); 
+  if (!errors.isEmpty()) {
+    console.log(errors.array())
+    return res.render('register', {title:'registration failed'});
+  }
 
-  let query = `INSERT INTO users (username, email, password) VALUES (?,?,?)`
-
+  // query starts here
+  const connection = require('../db.js') 
+  let query = `INSERT INTO users (username, email, password) VALUES (?,?,?)` 
   connection.query(query, [username, email, password], (err, results, fields)=>{
     if(err) throw err; 
     res.render('register', { title: 'registration completed' }); 
