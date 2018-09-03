953c4c76c670022519d336ab577da6d9fcc5798b
Apolo <apolo@192.168.0.102>
2018-09-03 21:50:47 +0800

    update template

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const router = express.Router(); 
+const router = express.Router();
 const bcrypt = require('bcrypt')
 const passport = require('passport')
 
 const { check, validationResult } = require('express-validator/check');
 
-/* GET home page. */ 
-router.get('/', (req, res, next) => { 
+/* GET home page. */
+router.get('/', (req, res, next) => {
   console.log("req.session.passport:", req.session.passport)
   console.log("req.session:", req.session)
   console.log("req.user:", req.user)
@@ -15,37 +15,47 @@ router.get('/', (req, res, next) => {
 });
 
 /* GET profile page. */
-router.get('/profile',authenticationMiddleware(),  (req, res, next) => { 
-  res.render('profile',  {title: 'Profile'}) 
+router.get('/profile', authenticationMiddleware(), (req, res, next) => {
+
+  // get userdata from database
+  const db = require('../db.js')
+
+  db.query('SELECT username FROM users where id=?', [req.user.user_id], (err, results, fields) => {
+    if (err) throw err;
+    else {
+      username = results[0].username 
+      res.render('profile', { username: username })
+    }
+  })
 })
 
 
 /* GET login page. */
-router.get('/login', (req, res, next) => { 
-  res.render('login',  {title: 'Login'}) 
+router.get('/login', (req, res, next) => {
+  res.render('login', { title: 'Login' })
 })
 
 
 /* POST login page. */
-router.post('/login', 
+router.post('/login',
   passport.authenticate(
-    'local',{
+    'local', {
       successRedirect: '/profile',
       failureRedirect: '/login'
     }
   )
-) 
+)
 
 /* GET Logot page. */
-router.get('/logout', (req, res, next) => { 
+router.get('/logout', (req, res, next) => {
   // user passport req.logout to delete session
   req.logout()
 
   res.clearCookie('connect.sid', { path: '/' });
-  req.session.destroy( (err)=>{
-    if(err) throw err
+  req.session.destroy((err) => {
+    if (err) throw err
     res.redirect('/')
-  }) 
+  })
 })
 
 
@@ -57,12 +67,12 @@ router.get('/register', (req, res, next) => {
 
 /* POST register user */
 router.post(
-  '/register', 
+  '/register',
   [
-    check('username','username must be between 8-16 characters').isLength({min: 8, max: 16}), 
+    check('username', 'username must be between 8-16 characters').isLength({ min: 8, max: 16 }),
     check('email').isEmail().withMessage('please input a valid email'),
-    check('email').isLength({min:8, max:100}).withMessage('email must be between 8-100 characters'),
-    check('password').isLength({min:5, max:100}).withMessage('password should be at lease 5 characters').custom((value,{req, loc, path}) => {
+    check('email').isLength({ min: 8, max: 100 }).withMessage('email must be between 8-100 characters'),
+    check('password').isLength({ min: 8, max: 100 }).withMessage('password should be at lease 8 characters').custom((value, { req, loc, path }) => {
       if (value !== req.body.passwordMatch) {
         // trow error if passwords do not match
         throw new Error("Passwords don't match");
@@ -71,72 +81,72 @@ router.post(
       }
     })
   ]
-  , 
-  (req, res, next)=>{
-    let {username, email, password, passwordMatch} = req.body 
+  ,
+  (req, res, next) => {
+    let { username, email, password, passwordMatch } = req.body
     console.log(username, email, password, passwordMatch)
 
     // get form data validation result
-    const errors = validationResult(req); 
+    const errors = validationResult(req);
     validationErrors = errors.array()
-    if (!errors.isEmpty()) { 
-      return res.render('register', {title:'registration failed', errors: validationErrors})
-    }   
+    if (!errors.isEmpty()) {
+      return res.render('register', { title: 'registration failed', errors: validationErrors })
+    }
 
-    else{
-      const db = require('../db.js') 
+    else {
+      const db = require('../db.js')
 
       // hash user password
       const saltRounds = 10
-      bcrypt.hash(password, saltRounds, (err, hashResult)=>{
+      bcrypt.hash(password, saltRounds, (err, hashResult) => {
 
         // create user in database
-        let query = `INSERT INTO users (username, email, password) VALUES (?,?,?)` 
-        db.query(query, [username, email, hashResult], (err, results, fields)=>{
-          if(err) { 
-            console.log('error happen during query:',err); 
-            res.render('register', { title: 'registration failed' }); 
-          } 
-          else{ 
+        let query = `INSERT INTO users (username, email, password) VALUES (?,?,?)`
+        db.query(query, [username, email, hashResult], (err, results, fields) => {
+          if (err) {
+            console.log('error happen during query:', err);
+            res.render('register', { title: 'registration failed' });
+          }
+          else {
             // login user after registration
-            db.query('SELECT LAST_INSERT_ID() as user_id', (err, results, fields)=>{
-              if(err) throw err;
+            db.query('SELECT LAST_INSERT_ID() as user_id', (err, results, fields) => {
+              if (err) throw err;
 
               let user_id = results[0]
-              console.log('user_id:',user_id) 
+              console.log('user_id:', user_id)
 
               // use passport req.login to log in user with user_id as session
-              req.login(user_id, (err) =>{
+              req.login(user_id, (err) => {
                 // redirect to homepage after login
                 res.redirect('/')
               })
             })
-          } 
-        }) 
-      }) 
+          }
+        })
+      })
     }
   });
 
 
 // serialize before store session
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user);
 });
 
 // deserialize before read from session
-passport.deserializeUser(function(user, done) {
-  done( null,user);
+passport.deserializeUser(function (user, done) {
+  done(null, user);
 });
 
 // define a middleware to authenticate user
-function authenticationMiddleware(){
-  return (req, res, next)=>{
+function authenticationMiddleware() {
+  return (req, res, next) => {
     // if user is authenticated (logged-in), then jump to next middleware
-    if(req.isAuthenticated()) return next();
+    if (req.isAuthenticated()) return next();
 
     // else jump to login page
-    res.redirect('login') 
-  } 
+    res.redirect('login')
+  }
 }
 
 module.exports = router;
